29d50f64fd14f50e16b4023eac9ab3574e2c4a20
dpanet <dpanet@hotmail.com>
2019-06-04 14:03:53 +0400

    homey js

---
+++ api.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+module.exports = [
+    {
+        method: 'GET',
+        path: '/',
+        public: true,
+        fn: (args, callback) => {
+        }
+    }
+];
---
+++ src/api.ts
@@ -0,0 +1,15 @@
+import Homey = require('homey');
+
+module.exports=[
+{
+    method:'GET',
+    path:'/',
+    public:true,
+    fn: (args:any,callback:any)=>
+    {
+    }
+
+
+}
+
+]
